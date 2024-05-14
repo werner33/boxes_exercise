f8d25ce4a0929238794a1ba5b94dee2a04ed34aa
werner33 <jordan@codetrack.dev>
2024-05-14 14:34:36 -0400

    pokemon api list

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,71 @@ import {useState} from 'react';
 
 import './App.css'
 
-function PromoCard ({title="This is the default title.", blueTitle, showButton}) {
-  return (
-    <div className="promo-card" >
-      <h3 style={{'color' : blueTitle ? 'blue' : 'black' }}>{title}</h3>
-      <p>This is a cool topic.</p>
-      {showButton && <button>Buy Me!</button> }
-    </div>
-  )
-}
+// function PromoCard ({title="This is the default title.", blueTitle, showButton}) {
+//   return (
+//     <div className="promo-card" >
+//       <h3 style={{'color' : blueTitle ? 'blue' : 'black' }}>{title}</h3>
+//       <p>This is a cool topic.</p>
+//       {showButton && <button>Buy Me!</button> }
+//     </div>
+//   )
+// }
+
+// SCSS and BEM
+// create a new app
+// layouts (rows of boxes, grid, single box centered)
+// review state (useState)
+// toggle effect (with conditional classes or styling)
+// select effect (one box is blue at a time)
+// toggle effect across six boxes
+// build simple component
+// made component customizable with props
+// fetching data 
+
+import PokemonCard from './components/pokemonCard/PokemonCard';
 
 function App() {
+
+  // this is the state representing our pokemon
+  // well update the state with setPokemon
+  const [pokemon, setPokemon] = useState([]);
+
+  // reach out to some API
+  // get some data
+  // make sure the data is what we want
+  // render that data to a component
+  // customize the component based on some part of the data
+
+  // HTTP: GET(read), POST (create), PUT (update), DELETE
+
+  // reach out across the web, to get some pokemon data
+  fetch('https://pokeapi.co/api/v2/pokemon?limit=10&offset=0')
+    .then((response) =>{
+      // convert the response to JSON
+      return response.json()
+    })
+    .then(data =>{
+      console.log("our response as JSON", data);
+      // update the hook (state) with our pokemon data
+      setPokemon(data.results);
+    })
+    .catch( error => {
+      console.error(error);
+    })
+
+
+    // iterate through each one and render
   return (
     <div className="promo-card-container">
-      <PromoCard title="This is a Great Deal!" showButton />
-      <PromoCard title="Come Visit!" blueTitle />
-      <PromoCard blueTitle showButton />
+      {pokemon.map((singlePokemon, index) => { // index  = 0, 1, 2, 3, 4, 5
+        return(
+          <PokemonCard 
+            key={index+1} 
+            singlePokemon={singlePokemon}
+            hasBlackBackground={index % 2 === 1 ? true : false}
+          /> 
+        )
+      })}
     </div>
   )
 }
---
+++ src/components/pokemonCard/PokemonCard.jsx
@@ -0,0 +1,14 @@
+
+
+function PokemonCard ({singlePokemon, hasBlackBackground}) {
+
+  // const capitalizedName = singlePokemon.name.charAt(0).toUpperCase() + singlePokemon.name.slice(1);
+
+  return (
+    <div className={hasBlackBackground ? "single-pokemon black-background" : "single-pokemon"}>
+      <h3 className="single-pokemon__name">{singlePokemon.name}</h3>
+    </div>
+  )
+}
+
+export default PokemonCard;
