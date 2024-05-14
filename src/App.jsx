effe3a9ec32127f9707f265c68f9a181e6b72b87
werner33 <jordan@codetrack.dev>
2024-05-14 12:01:56 -0400

    make components with props

--- src/App.jsx
+++ src/App.jsx
@@ -2,51 +2,24 @@ import {useState} from 'react';
 
 import './App.css'
 
-function App() {
-
-  const [activeID, setActiveID] = useState(null); // 1,2,3,4,5
-
-  // make 5 boxes 200x200 px with red background
-  // set boxes in row, with 20 px gap between each
-  // number each box from 1-5 with a centered white number
-  // when i click on a box, it turns blue
-  // when i click on any other box, it turns blue and all others are red
-  // (only one is blue at a time)
-
-  const setBoxToBlue = (event) => {
-    setActiveID(event.target.id);
-  }
-
+function PromoCard ({title="This is the default title.", blueTitle, showButton}) {
   return (
-    <div className="boxes">
-      <div 
-        className={ activeID === '1' ? "boxes__box active" : "boxes__box" }
-        id="1"
-        onClick={setBoxToBlue}
-      >1</div> 
-      <div 
-        className={ activeID === '2' ? "boxes__box active" : "boxes__box" }
-        id="2"
-        onClick={setBoxToBlue}
-      >2</div>
-      <div 
-        className={ activeID === '3' ? "boxes__box active" : "boxes__box" }
-        id="3"
-        onClick={setBoxToBlue}
-      >3</div>
-      <div 
-        className={ activeID === '4' ? "boxes__box active" : "boxes__box" }
-        id="4"
-        onClick={setBoxToBlue}
-      >4</div>
-      <div 
-        className={ activeID === '5' ? "boxes__box active" : "boxes__box" }
-        id="5"
-        onClick={setBoxToBlue}
-      >5</div>
+    <div className="promo-card" >
+      <h3 style={{'color' : blueTitle ? 'blue' : 'black' }}>{title}</h3>
+      <p>This is a cool topic.</p>
+      {showButton && <button>Buy Me!</button> }
     </div>
   )
+}
 
+function App() {
+  return (
+    <div className="promo-card-container">
+      <PromoCard title="This is a Great Deal!" showButton />
+      <PromoCard title="Come Visit!" blueTitle />
+      <PromoCard blueTitle showButton />
+    </div>
+  )
 }
 
 export default App;
