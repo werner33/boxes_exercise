8a5a4ad595f09ee6d6e18b98e6c52916367e2154
werner33 <jordan@codetrack.dev>
2024-05-16 14:54:31 -0400

    improve: link to pokemon deatil page

--- src/pages/PokemonDetailView.jsx
+++ src/pages/PokemonDetailView.jsx
@@ -1,11 +1,25 @@
+
+import {useState, useEffect} from 'react';
 import { useParams } from "react-router-dom";
 
 const PokemonDetailView = () => {
 
   const {pokemonName} = useParams();
 
+  const [pokemonData, setPokemonData] = useState({}); // empty obj
+
 
   // fetch data from the api for the specific pokemon in the url
+
+  useEffect(() => {
+
+    const pokemonURL = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+
+    fetch(pokemonURL)
+      .then(response => response.json())
+      .then(data => setPokemonData(data))
+      .catch(err => console.error(err.message));
+  },[]); // when component mounts, run the function inside
 
   // once data is processed to JSON, update state
 
@@ -13,6 +27,14 @@ const PokemonDetailView = () => {
     <div>
       Pokemon detail view for {pokemonName}
       {/* render out some information from state */}
+      <div> Weight: {pokemonData.weight} </div>
+
+      <div>Stats</div>
+      {pokemonData?.stats?.map((statObj, index) => {
+        return (
+          <div key={index}>{statObj?.stat?.name} : {statObj?.base_stat} </div>
+        )
+      })}
     </div>
   )
 }
